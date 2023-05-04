d2e33952146a307c3266dba50fc1a23d4959b6a0
zhangqiang <759567954@qq.com>
2023-05-04 11:48:42 +0800

    '第二次提交'

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 console.log("这是我的app.js文件");
+
+//初始化git  :  git init
+//查看git文件状态： git status
+//把文件从工作区提交到暂存区： git add 文件名 或者  git add .
+//把文件从暂存区提交到git本地仓库: git commit -m '提交信息'
+//查看提交记录：git log
